4c5790610abce08209ba27ee15eeb7bdde933db0
Krithika Ganesh <krithika.write@gmail.com>
2017-09-26 01:17:46 -0400

    circular list changes to create

--- pet_thread.c
+++ pet_thread.c
@@ -18,6 +18,7 @@
 
 
 
+
 typedef enum {PET_THREAD_STOPPED,
 	      PET_THREAD_RUNNING,
  	      PET_THREAD_READY,
@@ -44,15 +45,94 @@ struct exec_ctx {
 
 struct pet_thread {
     /* Implement this */
+    /*ULT*/
+    pet_thread_id_t thread_id;
+    thread_run_state_t state;
+    struct exec_ctx context;
+
 };
+
+struct node {
+    struct node *prev;
+    struct pet_thread *n;
+    struct node *next;
+}*head, *tail, *temp, *seachNode;
+
+int countNodes = 0;
+int numOfThreads = 1;
+int getcount() {
+    return countNodes;
+}
+
+//Return Head of Linked List
+struct pet_thread* returnHead() {
+    return (head->n);
+}
+
+//Return Tail of Linked List
+struct pet_thread* returnTail() {
+    return (tail->n);
+}
+
+//Create node creates node for LinkedList
+void create_node() {
+    struct pet_thread* data;
+    temp = (struct node *) malloc(1 * sizeof(struct node));
+    temp->prev = NULL;
+    temp->next = NULL;
+    countNodes++;
+}
+
+//Insert node at the end of LinkedList
+void enqueue(struct pet_thread* data) {
+    if (head == NULL) { //for the first node
+        create_node();
+        head = temp;
+        tail = temp;
+    } else {
+        create_node();
+        tail->next = temp;
+        temp->prev = tail;
+        tail = temp;
+        tail->next = head;
+        head->prev = tail;
+    }
+    temp->n = data;
+}
+
+struct pet_thread* searchNextActiveThread(pet_thread_id_t threadId) {
+    //First search from head to find threadId
+    temp = head;
+
+    while (temp != NULL) {
+        if (temp->n->thread_id == threadId) {
+            //seachNode=temp;
+            break;
+        } else {
+            temp = temp->next;
+        }
+    }
+    if (temp->next != NULL) {
+        temp = temp->next;
+        while (temp != NULL) {
+            if (temp->n->state == PET_THREAD_READY) {
+                return temp->n;
+            } else {
+                temp = temp->next;
+            }
+        }
+
+    } else {
+        exit(0);
+    }
+
+
+}
 
 
 
 static pet_thread_id_t current     = PET_MASTER_THREAD_ID;
-struct pet_thread      master_dummy_thread;
-
-static LIST_HEAD(thread_list);
-
+struct pet_thread      *master_dummy_thread;
 
 extern void __switch_to_stack(void            * tgt_stack,
 			      void            * saved_stack,
@@ -66,25 +146,35 @@ get_thread(pet_thread_id_t thread_id)
 {
 
     if (thread_id == PET_MASTER_THREAD_ID) {
-	return &(master_dummy_thread);
+	return master_dummy_thread;
+    }
+    /* Implement this */
+
+    /*need to search for the thread id and return*/
+    else {
+        temp = head;
+        while (temp != NULL) {
+            if (temp->n->thread_id == thread_id)
+                return (temp->n);
+            else {
+                temp = temp->next;
+            }
+        }
     }
 
-
-    /* Implement this */
-    
-    return NULL;
+    //return NULL;
 }
 
 static pet_thread_id_t
 get_thread_id(struct pet_thread * thread)
 {
-    if (thread == &(master_dummy_thread)) {
+    if (thread == master_dummy_thread) {
 	return PET_MASTER_THREAD_ID;
     }
-
+    else
     /* Implement this */
-    
-    return 0;
+        /*ULT*/
+        return thread->thread_id;
 }
 
 
@@ -94,6 +184,7 @@ pet_thread_init(void)
     printf("Initializing Pet Thread library\n");
 
     /* Implement this */
+
     
     return 0;
 }
@@ -145,17 +236,32 @@ pet_thread_create(pet_thread_id_t * thread_id,
 		  pet_thread_fn_t   func,
 		  void            * arg)
 {
-    struct pet_thread * new_thread;
+    struct pet_thread *new_thread;
     
     /* Implement this */
     
+
+    if (getcount() == 0) {
+        //Create master_dummy_thread
+        master_dummy_thread->thread_id = PET_MASTER_THREAD_ID;
+        //HOW TO CREATE CONTEXT
+        master_dummy_thread->state = PET_THREAD_READY;
+        enqueue(master_dummy_thread);
+    }
+    //HOW TO CREATE CONTEXT
+    (*new_thread).state = PET_THREAD_READY;
+    new_thread->thread_id = numOfThreads++;
+    //HOW TO CHANGE CONTEXT
+    enqueue(new_thread);
+
     DEBUG("Created new Pet Thread (%p):\n", new_thread);
     DEBUG("--Add thread state here--\n");
-    __dump_stack(new_thread);
+    __dump_stack(new_thread);//what is this?
 
-    
     return 0;
 }
+    
+
 
 
 void
