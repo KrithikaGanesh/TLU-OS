de9a7eafe7782c9ac67760bfc6eeedc8074b1db5
akhilyendluri <akhilyendluri@gmail.com>
2017-10-07 23:47:05 -0400

    update pet_thread.c
    Implemented Context Switch in pet_thread_create

--- pet_thread.c
+++ pet_thread.c
@@ -19,9 +19,9 @@
 
 
 typedef enum {PET_THREAD_STOPPED,
-	      PET_THREAD_RUNNING,
- 	      PET_THREAD_READY,
-	      PET_THREAD_BLOCKED} thread_run_state_t;
+              PET_THREAD_RUNNING,
+              PET_THREAD_READY,
+              PET_THREAD_BLOCKED} thread_run_state_t;
 
 struct exec_ctx {
     uint64_t rbp;
@@ -46,23 +46,22 @@ struct pet_thread {
     struct exec_ctx context;
     pet_thread_id_t thread_id;
     thread_run_state_t state;
-    void * stackPtr;
-    /* Implement this */
+    char * stackPtr;
+    struct list_head node;
 };
-
 
 static pet_thread_id_t current     = PET_MASTER_THREAD_ID;
 struct pet_thread      master_dummy_thread;
 
 static LIST_HEAD(thread_list);
-static struct list_head readyList;
+struct pet_thread readyQueue;
 static int thread_ids = 1;
 struct pet_thread * master_thread;
 
 extern void __switch_to_stack(void            * tgt_stack,
-			      void            * saved_stack,
-			      pet_thread_id_t   current,
-			      pet_thread_id_t   tgt);
+                              void            * saved_stack,
+                              pet_thread_id_t   current,
+                              pet_thread_id_t   tgt);
 
 extern void __abort_to_stack(void * tgt_stack);
 
@@ -71,13 +70,12 @@ get_thread(pet_thread_id_t thread_id)
 {
 
     if (thread_id == PET_MASTER_THREAD_ID) {
-	return &(master_dummy_thread);
+        return &(master_dummy_thread);
     }
+
+
     /* Implement this */
 
-    /*need to search for the thread id and return*/
-
-    
     return NULL;
 }
 
@@ -85,25 +83,25 @@ static pet_thread_id_t
 get_thread_id(struct pet_thread * thread)
 {
     if (thread == &(master_dummy_thread)) {
-	return PET_MASTER_THREAD_ID;
+        return PET_MASTER_THREAD_ID;
     }
-    else
+
     /* Implement this */
-        /*ULT*/
-        return thread->thread_id;
+
+    return 0;
 }
 
 
 int
 pet_thread_init(void)
 {
-    printf("Initializing Pet Thread library\n");
-    master_thread = (struct pet_thread *)malloc(sizeof(struct pet_thread));
+    INIT_LIST_HEAD(&readyQueue.node);
+    master_thread = (struct pet_thread *)calloc(1,sizeof(struct pet_thread));
     master_thread->thread_id = PET_MASTER_THREAD_ID;
+    printf("master thread = %d\n",(int)master_thread->thread_id);
+    master_thread->stackPtr = calloc(1,STACK_SIZE);
+    list_add_tail(&(master_thread->node),&(readyQueue.node));
     printf("Initializing Pet Thread library\n");
-    //list_head_init(&readyList);
-    /* Implement this */
-
     return 0;
 }
 
@@ -113,7 +111,7 @@ __dump_stack(struct pet_thread * thread)
 {
 
     /* Implement this */
-    
+
     return;
 }
 
@@ -121,11 +119,11 @@ __dump_stack(struct pet_thread * thread)
 
 int
 pet_thread_join(pet_thread_id_t    thread_id,
-		void            ** ret_val)
+                void            ** ret_val)
 {
 
     /* Implement this */
-    
+
     return 0;
 }
 
@@ -142,10 +140,10 @@ static int
 __thread_invoker(struct pet_thread * thread)
 {
 
-    /* Implement this */
-    
+    printf("Inside Invoker");
+
     return 0;
-    
+
 }
 
 
@@ -156,26 +154,31 @@ pet_thread_create(pet_thread_id_t * thread_id,
 {
     struct pet_thread * new_thread;
     new_thread = (struct pet_thread *)malloc(sizeof(struct pet_thread));
-    new_thread->thread_id = (pet_thread_id_t)thread_ids++;
+    new_thread->thread_id = (pet_thread_id_t)++thread_ids;
+    printf("new_thread->thread_id = %d\n",(int)new_thread->thread_id);
     new_thread->state = PET_THREAD_READY;
-    new_thread->stackPtr = &func;
-    __switch_to_stack(new_thread->stackPtr,master_thread->stackPtr,new_thread->thread_id,master_thread->thread_id);
-    /* Implement this */
-
+    new_thread->stackPtr = calloc(1,STACK_SIZE);
+    char * size = (new_thread->stackPtr + STACK_SIZE);
+    new_thread->stackPtr = size;
+    struct exec_ctx *cont = size;
+    cont->rip = (uint64_t)__thread_invoker;
+    list_add_tail(&(new_thread->node),&(readyQueue.node));
+    __switch_to_stack(&new_thread->stackPtr,&master_thread->stackPtr,new_thread->thread_id,master_thread->thread_id);
     DEBUG("Created new Pet Thread (%p):\n", new_thread);
     DEBUG("--Add thread state here--\n");
-    __dump_stack(new_thread);
+//    __dump_stack(new_thread);
 
 
     return 0;
 }
 
+
 void
 pet_thread_cleanup(pet_thread_id_t prev_id,
-		   pet_thread_id_t my_id)
+                   pet_thread_id_t my_id)
 {
-    /* Implement this */
-    
+    printf("cleanup prev_id = %d\tmy_id = %d"+prev_id,my_id);
+
 }
 
 
@@ -205,7 +208,7 @@ pet_thread_schedule()
 {
 
     /* Implement this */
-    
+
     return 0;
 }
 
@@ -217,14 +220,18 @@ pet_thread_schedule()
 int
 pet_thread_run()
 {
-
+    struct list_head *pos;
+    struct pet_thread *tmp;
     printf("Starting Pet Thread execution\n");
-    
-    
+    list_for_each(pos,&readyQueue.node) {
+       tmp = (struct pet_thread *)malloc(sizeof(struct pet_thread));
+       tmp = list_entry(pos,struct pet_thread,node);
+       printf("id = %d\n",(int)tmp->thread_id);
+    }
+
     pet_thread_schedule();
-    
+
     printf("Pet Thread execution has finished\n");
 
     return 0;
 }
-	     
